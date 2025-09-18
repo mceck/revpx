7607301f3391e4d8e5cf6889eefd960be3257238
mceck <macecco@gmail.com>
2025-09-18 14:15:40 +0200

    ep

--- src/ep.h
+++ src/ep.h
@@ -1,45 +1,32 @@
+#ifndef _EP_H
+#define _EP_H
+
 #ifndef __APPLE__
 #include <sys/epoll.h>
 #else
-#include <stdlib.h>
+#include <sys/types.h>
 #include <sys/event.h>
 #include <sys/time.h>
-#endif
+#include <unistd.h>
+#include <errno.h>
 
-#ifdef __APPLE__
-// Minimal epoll compatibility layer for macOS using kqueue
-#ifndef EPOLLIN
+// Map epoll constants to kqueue equivalents
 #define EPOLLIN 0x001
-#endif
-#ifndef EPOLLOUT
 #define EPOLLOUT 0x004
-#endif
-#ifndef EPOLLERR
 #define EPOLLERR 0x008
-#endif
-#ifndef EPOLLHUP
 #define EPOLLHUP 0x010
-#endif
-// EPOLLET is edge-triggered; with kqueue we emulate via EV_CLEAR. Flag value is unused.
-#ifndef EPOLLET
-#define EPOLLET 0x000
-#endif
+#define EPOLLET 0x020 // just a dummy flag, EV_CLEAR handles edge-triggered
 
-#ifndef EPOLL_CTL_ADD
 #define EPOLL_CTL_ADD 1
-#endif
-#ifndef EPOLL_CTL_DEL
 #define EPOLL_CTL_DEL 2
-#endif
-#ifndef EPOLL_CTL_MOD
 #define EPOLL_CTL_MOD 3
-#endif
 
 struct epoll_event {
-    uint32_t events;
+    uint32_t events; // EPOLLIN, EPOLLOUT, etc.
     union {
-        int fd;
         void *ptr;
+        int fd;
+        uint32_t u32;
         uint64_t u64;
     } data;
 };
@@ -49,85 +36,50 @@ static inline int epoll_create1(int flags) {
     return kqueue();
 }
 
-static inline int epoll_ctl(int epfd, int op, int fd, struct epoll_event *ev) {
-    struct kevent changes[2];
-    int nchanges = 0;
-    uint16_t base_flags = EV_ADD; // Use level-triggered semantics
+static inline int epoll_ctl(int kq, int op, int fd, struct epoll_event *ev) {
+    struct kevent kev[2];
+    int n = 0;
 
     if (op == EPOLL_CTL_ADD || op == EPOLL_CTL_MOD) {
-        if (ev && (ev->events & EPOLLIN)) {
-            EV_SET(&changes[nchanges++], (uintptr_t)fd, EVFILT_READ, base_flags, 0, 0, NULL);
-        } else if (op == EPOLL_CTL_MOD) {
-            EV_SET(&changes[nchanges++], (uintptr_t)fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+        if (ev->events & EPOLLIN) {
+            EV_SET(&kev[n++], fd, EVFILT_READ, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, NULL);
         }
-
-        if (ev && (ev->events & EPOLLOUT)) {
-            EV_SET(&changes[nchanges++], (uintptr_t)fd, EVFILT_WRITE, base_flags, 0, 0, NULL);
-        } else if (op == EPOLL_CTL_MOD) {
-            EV_SET(&changes[nchanges++], (uintptr_t)fd, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
+        if (ev->events & EPOLLOUT) {
+            EV_SET(&kev[n++], fd, EVFILT_WRITE, EV_ADD | EV_ENABLE | EV_CLEAR, 0, 0, NULL);
         }
-
-        if (nchanges > 0) {
-            return kevent(epfd, changes, nchanges, NULL, 0, NULL);
-        }
-        return 0;
     } else if (op == EPOLL_CTL_DEL) {
-        EV_SET(&changes[0], (uintptr_t)fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-        EV_SET(&changes[1], (uintptr_t)fd, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
-        // kevent with EV_DELETE will return 0 if filter is successfully removed,
-        // or -1 with ENOENT if it does not exist. In either case, we can
-        // ignore the return value for a simple DEL operation.
-        (void)kevent(epfd, changes, 2, NULL, 0, NULL);
-        return 0;
-    } else {
-        errno = EINVAL;
-        return -1;
+        EV_SET(&kev[n++], fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+        EV_SET(&kev[n++], fd, EVFILT_WRITE, EV_DELETE, 0, 0, NULL);
     }
+
+    return kevent(kq, kev, n, NULL, 0, NULL);
 }
 
-static inline int epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout) {
-    if (maxevents <= 0) {
-        errno = EINVAL;
-        return -1;
-    }
+static inline int epoll_wait(int kq, struct epoll_event *evs, int maxevents, int timeout) {
+    struct kevent kev[maxevents];
+    struct timespec ts, *pts = NULL;
 
-    struct timespec ts, *tsp = NULL;
     if (timeout >= 0) {
         ts.tv_sec = timeout / 1000;
         ts.tv_nsec = (timeout % 1000) * 1000000;
-        tsp = &ts;
+        pts = &ts;
     }
 
-    // Allocate evlist dynamically to respect maxevents
-    struct kevent *evlist = (struct kevent *)calloc((size_t)maxevents, sizeof(struct kevent));
-    if (!evlist) {
-        errno = ENOMEM;
-        return -1;
+    int n = kevent(kq, NULL, 0, kev, maxevents, pts);
+    if (n <= 0) return n;
+
+    for (int i = 0; i < n; i++) {
+        evs[i].data.fd = (int)kev[i].ident;
+        evs[i].events = 0;
+
+        if (kev[i].filter == EVFILT_READ) evs[i].events |= EPOLLIN;
+        if (kev[i].filter == EVFILT_WRITE) evs[i].events |= EPOLLOUT;
+        if (kev[i].flags & EV_ERROR) evs[i].events |= EPOLLERR;
+        if (kev[i].flags & EV_EOF) evs[i].events |= EPOLLHUP;
     }
 
-    int n = kevent(epfd, NULL, 0, evlist, maxevents, tsp);
-    if (n > 0) {
-        for (int i = 0; i < n; i++) {
-            events[i].data.fd = (int)evlist[i].ident;
-            uint32_t mask = 0;
-            if (evlist[i].filter == EVFILT_READ) {
-                mask |= EPOLLIN;
-                if (evlist[i].flags & EV_EOF) {
-                    // Read side reached EOF: signal HUP but allow draining with EPOLLIN
-                    mask |= EPOLLHUP;
-                }
-            }
-            if (evlist[i].filter == EVFILT_WRITE) {
-                mask |= EPOLLOUT;
-                // Do NOT map EV_EOF on write filter to EPOLLHUP; it only means peer won't read more
-                // and will be surfaced as write error (EPIPE) on write attempt.
-            }
-            if (evlist[i].flags & EV_ERROR) mask |= EPOLLERR;
-            events[i].events = mask;
-        }
-    }
-
-    free(evlist);
     return n;
 }
 #endif // __APPLE__
+
+#endif // _EP_H
