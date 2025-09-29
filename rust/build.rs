55b35a82e1db901dbc23acd3d6479fbe070cbf17
Mattia Cecchini <matcecco@gmail.com>
2025-09-30 00:55:54 +0200

    pkg-config

--- rust/build.rs
+++ rust/build.rs
@@ -1,14 +1,14 @@
 fn main() {
     let mut lib = cc::Build::new();
     lib.file("../src/revpx-lib.c");
-    #[cfg(target_os = "macos")]
-    {
-        let openssl_include = "/opt/homebrew/include/";
-        lib.include(openssl_include);
-        println!("cargo:include={}", openssl_include);
-        println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
+
+    if let Ok(openssl) = pkg_config::Config::new().probe("openssl") {
+        for include in openssl.include_paths {
+            lib.include(include);
+        }
+    } else {
+        panic!("Could not find OpenSSL installation via pkg-config");
     }
+
     lib.compile("revpx");
-    println!("cargo:rustc-link-lib=crypto");
-    println!("cargo:rustc-link-lib=ssl");
 }
